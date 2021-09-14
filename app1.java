cd416ecf9e48330d3f9b8f495010368462554ea3
Safronov Evgeniy <safronov.evgenijon@mail.ru>
2021-09-14 20:14:04 +0300

    some work in branch

--- app1.java
+++ app1.java
@@ -3,9 +3,6 @@ public class app1
 	public static void main(String[] args) 
 	{
 		System.out.println("Otchislenie iz MTUCI");
-		System.out.println("+zachet po laboratornoy");
-		System.out.println("Bespoleznaya stroka");
-		System.out.println("RBPO one love");
 	}
 }
 
